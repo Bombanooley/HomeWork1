4f9340cd6e6fcf15904f7d5287d7059fca89a250
Bombanooley <Bombanooley@yandex.ru>
2020-09-19 12:51:20 +0400

    Второе упражнение

---
+++ HomeWork7/Exercise 2/Form2.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Exercise_2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.btOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(91, 87);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(181, 38);
+            this.tbInput.TabIndex = 0;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblText.Location = new System.Drawing.Point(144, 47);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(76, 25);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "lblText";
+            // 
+            // btOK
+            // 
+            this.btOK.Location = new System.Drawing.Point(121, 150);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(123, 37);
+            this.btOK.TabIndex = 2;
+            this.btOK.Text = "btOK";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 210);
+            this.ControlBox = false;
+            this.Controls.Add(this.btOK);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Загадай число";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Button btOK;
+    }
+}
---
+++ HomeWork7/Exercise 2/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_2
+{
+    public partial class Form2 : Form
+    {
+        Form1 mf;
+        int input, key;
+        public Form2(Form1 f)
+        {
+            InitializeComponent();
+            mf = f;
+            lblText.Text = "Введите число";
+            btOK.Text = "OK";
+            lblText.Left = (this.Size.Width - lblText.Width) / 2;
+            tbInput.Left = (this.Size.Width - tbInput.Width) / 2;
+            btOK.Left = (this.Size.Width - btOK.Width) / 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flag = int.TryParse(tbInput.Text, out input);
+            if (flag)
+            {
+                key = Form1.key;
+                tbInput.Text = null;
+                this.Visible = false;
+                if (input > key) mf.lblAnsw.Text = "Меньше";
+                else if (input < key) mf.lblAnsw.Text = "Больше";
+                else
+                {
+                    mf.lblAnsw.Text = "Ты угадал!";
+                    mf.btTry.Enabled = false;
+                }
+                mf.lblAnsw.Left = (mf.Size.Width - mf.lblAnsw.Width) / 2;
+            }
+        }
+    }
+}
